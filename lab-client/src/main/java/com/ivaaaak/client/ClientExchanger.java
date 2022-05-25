f3d026e84b0e2212bb96c4208ad09e42d8763c44
eva <ivaaaaak@mail.ru>
2022-05-25 20:46:26 +0300

    полное чтение и запись в неблокирующем режиме

--- lab-client/src/main/java/com/ivaaaak/client/ClientExchanger.java
+++ lab-client/src/main/java/com/ivaaaak/client/ClientExchanger.java
@@ -18,6 +18,7 @@ public class ClientExchanger {
     private final String host;
     private final int port;
     private final int maxMetaData = 4;
+    private final int clientWaitingPeriod = 7;
     private SocketChannel channel;
 
     public ClientExchanger(String host, int port) {
@@ -25,55 +26,74 @@ public class ClientExchanger {
         this.port = port;
     }
 
+    public void setSocketChannel(SocketChannel newChannel) {
+        channel = newChannel;
+    }
+
     public SocketChannel openChannelToServer() throws IOException {
         SocketChannel newChannel = SocketChannel.open();
         InetAddress ia = InetAddress.getByName(host);
         InetSocketAddress address = new InetSocketAddress(ia, port);
         newChannel.connect(address);
-        System.out.println("Connection has been established");
+        System.out.println("Connection with server " + ia.getHostName() + " has been established");
         newChannel.configureBlocking(false);
         return newChannel;
     }
 
-    public void setSocketChannel(SocketChannel newChannel) {
-        channel = newChannel;
-    }
-
     public SocketChannel reconnectToServer() {
         try {
-            System.err.println("Failed to exchange data with server. Server isn't working");
             return openChannelToServer();
         } catch (IOException e) {
             return null;
         }
     }
 
-    public void sendCommand(Command command) throws IOException {
+    public void sendCommand(Command command) throws IOException, InterruptedException {
         byte[] serializedCommand = serialize(command);
         int commandSize = serializedCommand.length;
 
         ByteBuffer mainData = ByteBuffer.wrap(serializedCommand);
         ByteBuffer metaData = ByteBuffer.allocate(maxMetaData).putInt(commandSize);
         metaData.position(0);
-        channel.write(metaData);
-        channel.write(mainData);
-    }
 
-    public CommandResult receiveResult() throws IOException, ClassNotFoundException, InterruptedException {
-        ByteBuffer metaData = ByteBuffer.allocate(maxMetaData);
-        final int clientWaitingPeriod = 10;
         int waitingTime = clientWaitingPeriod;
         while (waitingTime > 0) {
-            if (channel.read(metaData) != 0) {
-                metaData.position(0);
-                ByteBuffer mainData = ByteBuffer.allocate(metaData.getInt());
-                channel.read(mainData);
-                return (CommandResult) deserialize(mainData.array());
+            if (channel.write(metaData) == metaData.remaining()) {
+                while (waitingTime > 0) {
+                    if (channel.write(mainData) != mainData.remaining()) {
+                        waitingTime--;
+                        TimeUnit.MILLISECONDS.sleep(clientWaitingPeriod);
+                    } else {
+                        return;
+                    }
+                }
             }
             waitingTime--;
             TimeUnit.MILLISECONDS.sleep(clientWaitingPeriod);
         }
-        return new CommandResult("Waiting time for the response from server has expired", null);
+        System.err.println("Failed to send the command");
+    }
+
+    public CommandResult receiveResult() throws IOException, ClassNotFoundException, InterruptedException {
+        ByteBuffer metaData = ByteBuffer.allocate(maxMetaData);
+
+        int waitingTime = clientWaitingPeriod;
+        while (waitingTime > 0) {
+            if (channel.read(metaData) == metaData.remaining()) {
+                metaData.position(0);
+                ByteBuffer mainData = ByteBuffer.allocate(metaData.getInt());
+                while (waitingTime > 0) {
+                    if (channel.read(mainData) == mainData.remaining()) {
+                        return (CommandResult) deserialize(mainData.array());
+                    }
+                    waitingTime--;
+                    TimeUnit.MILLISECONDS.sleep(clientWaitingPeriod);
+                }
+            }
+            waitingTime--;
+            TimeUnit.MILLISECONDS.sleep(clientWaitingPeriod);
+        }
+        return new CommandResult("Waiting time for the response from server has expired");
     }
 
 
